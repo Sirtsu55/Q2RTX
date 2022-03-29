ce49c160ce6561693bbf23ab50b7a92fb9ccc10d
PalmliX <adamearlpalmer@hotmail.com>
2022-03-29 18:43:07 -0400

    Increased Axe regular attack damage Decreased shotgun damage Increased Knight health

--- src/basenac/weapons/nac_shotgun.c
+++ src/basenac/weapons/nac_shotgun.c
@@ -27,7 +27,7 @@ static void Shotgun_Fire(edict_t *ent)
     vec3_t      start;
     vec3_t      forward, right;
     vec3_t      offset;
-    int         damage = 4;
+    int         damage = 2;
     int         kick = 1;
 
     AngleVectors(ent->client->v_angle, forward, right, NULL);
