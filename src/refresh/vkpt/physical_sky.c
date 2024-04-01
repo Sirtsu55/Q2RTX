2da8d0b3c184e3761d05268bbfcddfbd85344593
Srijan Dhungana <srijan.dhungana6@gmail.com>
2024-04-01 15:08:56 +0300

    rescale sun_brightness to be same intensity as with old version

--- src/refresh/vkpt/physical_sky.c
+++ src/refresh/vkpt/physical_sky.c
@@ -913,7 +913,7 @@ vkpt_evaluate_sun_light(sun_light_t* light, const vec3_t sky_matrix[3], float ti
 
 	// color before occlusion
 	vec3_t sunColor = { sun_color[0]->value, sun_color[1]->value, sun_color[2]->value };
-	VectorScale(sunColor, (sun_brightness->value), light->color);
+	VectorScale(sunColor, (sun_brightness->value * 10), light->color);
 
 	// potentially visible - can be overridden if readback data says it's occluded
 	if (physical_sky_space->integer)
